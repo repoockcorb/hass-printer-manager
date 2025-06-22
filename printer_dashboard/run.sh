6ea960db2c0a17a75ad233c2af0548a410ce9011
Brock Cooper <64189018+repoockcorb@users.noreply.github.com>
2025-06-23 00:14:36 +1000

    Update

--- printer_dashboard/run.sh
+++ printer_dashboard/run.sh
@@ -52,10 +52,10 @@ EOF
 
 location /proxy/${NAME}/ {
     proxy_pass         http://${NAME}_up/;
-    proxy_set_header   Host $host;
-    proxy_set_header   X-Real-IP $remote_addr;
-    proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
-    proxy_set_header   X-Forwarded-Proto $scheme;
+    proxy_set_header   Host \$host;
+    proxy_set_header   X-Real-IP \$remote_addr;
+    proxy_set_header   X-Forwarded-For \$proxy_add_x_forwarded_for;
+    proxy_set_header   X-Forwarded-Proto \$scheme;
     proxy_http_version 1.1;
     proxy_buffering    off;
 }
